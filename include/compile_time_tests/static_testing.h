958235ac8db9afeb7a3d6128be11a3cd6541aa56
Julian Becker <becker.julian@gmail.com>
2014-10-25 15:17:37 +0200

    added compile time testing helper assert_none

--- include/compile_time_tests/static_testing.h
+++ include/compile_time_tests/static_testing.h
@@ -51,13 +51,48 @@ namespace testing {
 
     //! @endcond
     
+    
+    
+    /// @brief metafunction to evaluate a list of individual compile-time tests
+    ///        and returns true if any one of them succeeds
+    /// @tparam RESULTS a list of constexpr booleans
+    /// @return true if any RESULT is true
+    template <bool...RESULTS> struct
+    assert_none {
+        /// the static return value. Is true if none of the RESULTS is true.
+        public: static constexpr bool
+        value = !assert_any<RESULTS...>::value;
+    };
+    
     namespace __testing {
+        // test assert_all
         static_assert(assert_all<true>::value,"error");
         static_assert(!assert_all<false>::value,"error");
         static_assert(assert_all<true,true>::value,"error");
-        static_assert(assert_all<true,true,true>::value,"error");
+        static_assert(!assert_all<false,false,false>::value,"error");
         static_assert(!assert_all<true,true,false,true>::value,"error");
         static_assert(!assert_all<false,true,true,true>::value,"error");
         static_assert(!assert_all<true,true,true,false>::value,"error");
+        
+        // test assert_any
+        static_assert(assert_any<true>::value,"error");
+        static_assert(!assert_any<false>::value,"error");
+        static_assert(assert_any<true,true>::value,"error");
+        static_assert(!assert_any<false,false,false>::value,"error");
+        static_assert(assert_any<true,true,true>::value,"error");
+        static_assert(assert_any<true,true,false,true>::value,"error");
+        static_assert(assert_any<false,true,true,true>::value,"error");
+        static_assert(assert_any<true,true,true,false>::value,"error");
+        
+        // test assert_none
+        static_assert(!assert_none<true>::value,"error");
+        static_assert(assert_none<false>::value,"error");
+        static_assert(!assert_none<true,true>::value,"error");
+        static_assert(assert_none<false,false,false>::value,"error");
+        static_assert(!assert_none<true,true,true>::value,"error");
+        static_assert(!assert_none<true,true,false,true>::value,"error");
+        static_assert(!assert_none<false,true,true,true>::value,"error");
+        static_assert(!assert_none<true,true,true,false>::value,"error");
+        
     };
 }
