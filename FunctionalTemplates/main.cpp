581c4bbdfeb0a4a2b889584831853a6e1551196a
Julian Becker <becker.julian@gmail.com>
2014-10-25 12:52:23 +0200

    Initial Commit

---
+++ FunctionalTemplates/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FunctionalTemplates
+//
+//  Created by Julian Becker on 25.10.14.
+//  Copyright (c) 2014 Julian Becker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
