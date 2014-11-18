00aa037e1c6f4d4e63352e0bd43a5b5b0bd75e22
Julian Becker <becker.julian@gmail.com>
2014-11-18 22:45:51 +0100

    moved implementation of list::join into a separate include file

---
+++ include/list/detail/dtl_join.h
@@ -0,0 +1,14 @@
+//
+//  dtl_join.h
+//  FunctionalTemplates
+//
+//  Created by Julian Becker on 18.11.14.
+//  Copyright (c) 2014 Julian Becker. All rights reserved.
+//
+
+#ifndef FunctionalTemplates_dtl_join_h
+#define FunctionalTemplates_dtl_join_h
+
+
+
+#endif
