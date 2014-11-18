cca279656a7de4ba97ec64bb6837102a1b05e9bd
Julian Becker <becker.julian@gmail.com>
2014-11-18 21:42:33 +0100

    added .gitignore

---
+++ DoxygenDocs.docset/html/com.mycompany.DoxygenExample.docset/Contents/Resources/Documents/dir_42761cbf96bc5660f72a2d1a926765c0.js
@@ -0,0 +1,4 @@
+var dir_42761cbf96bc5660f72a2d1a926765c0 =
+[
+    [ "has_operator.h", "has__operator_8h_source.html", null ]
+];
---
+++ include/index_of.h
@@ -0,0 +1,14 @@
+//
+//  index_of.h
+//  FunctionalTemplates
+//
+//  Created by Julian Becker on 29.10.14.
+//  Copyright (c) 2014 Julian Becker. All rights reserved.
+//
+
+#ifndef FunctionalTemplates_index_of_h
+#define FunctionalTemplates_index_of_h
+
+
+
+#endif
