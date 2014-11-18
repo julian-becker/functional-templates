30c1338bb2bb23502790e74f6e183c1070530320
Julian Becker <becker.julian@gmail.com>
2014-11-18 18:59:14 +0100

    using unsigned integers instead of signed ints in list::length. added implementation in meta_types.h

--- include/list/length.h
+++ include/list/length.h
@@ -25,7 +25,7 @@ namespace list {
        
         template <template <typename...> class LIST_TYPE,typename...TS> struct
         __length<LIST_TYPE<TS...>> {
-            using result = meta_types::int_<sizeof...(TS)>;
+            using result = meta_types::uint_<sizeof...(TS)>;
         };
     }
     // ! @endcond
@@ -33,8 +33,8 @@ namespace list {
     template <typename LIST> using
     length = result_of<__dtl::__length<LIST>>;
     
-    ASSERT_EQUAL(length<list<int,double,wchar_t>>,meta_types::int_<3>);
-    ASSERT_EQUAL(length<list<>>,meta_types::int_<0>);
+    ASSERT_EQUAL(length<list<int,double,wchar_t>>,meta_types::uint_<3>);
+    ASSERT_EQUAL(length<list<>>,meta_types::uint_<0>);
 }
 
 #endif /* defined(__FunctionalTemplates__length__) */
--- include/meta_types/meta_types.h
+++ include/meta_types/meta_types.h
@@ -10,9 +10,20 @@
 #define __FunctionalTemplates__meta_types__
 
 namespace meta_types {
+
+    /// @brief wraps an int some_int into a type int_<some_int>
     template <int VALUE> struct
     int_{
+        /// the actual value
         static constexpr int value = VALUE;
+    };
+    
+    
+    /// @brief wraps an unsigned int some_uint into a type uint_<some_uint>
+    template <unsigned int VALUE> struct
+    uint_{
+        /// the actual value
+        static constexpr unsigned int value = VALUE;
     };
 }
 
