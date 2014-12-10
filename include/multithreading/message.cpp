f501393565eae5c589ef2d557e225e2f69d0c13c
Julian Becker <becker.julian@gmail.com>
2014-12-10 21:28:20 +0100

    added message class that can be used to encapsulate arbitrary types into a uniform „message“. This „message“ may be used in lists, queues and other communication techniques

---
+++ include/multithreading/message.cpp
@@ -0,0 +1,9 @@
+//
+//  message.cpp
+//  FunctionalTemplates
+//
+//  Created by Julian Becker on 10.12.14.
+//  Copyright (c) 2014 Julian Becker. All rights reserved.
+//
+
+#include "message.h"
---
+++ include/multithreading/message.h
@@ -0,0 +1,86 @@
+//
+//  message.h
+//  FunctionalTemplates
+//
+//  Created by Julian Becker on 10.12.14.
+//  Copyright (c) 2014 Julian Becker. All rights reserved.
+//
+
+#ifndef __FunctionalTemplates__message__
+#define __FunctionalTemplates__message__
+
+
+#include <tuple>
+#include <type_traits>
+#include <callable/callable.h>
+
+namespace
+multithreading {
+
+    struct
+    message {
+        private: struct
+        message_base {
+            virtual ~message_base() throw() {}
+        };
+
+        private: template <typename...TS> struct
+        message_wrapper : message_base {
+            const std::tuple<TS...> content;
+            explicit message_wrapper(TS&&...content) : content{content...} {}
+            explicit message_wrapper(const TS&...content) : content{content...} {}
+            virtual ~message_wrapper() throw() {}
+        };
+        
+        public: message_base const * msg;
+        
+        public: template <typename...TS>
+        message(const TS&...content)
+        : msg(new message_wrapper<TS...>(content...))
+        {}
+        
+        public: template <typename...TS>
+        explicit
+        message(TS&&...content)
+        : msg(new message_wrapper<TS...>(std::move(content)...))
+        {
+        }
+        
+        public:
+        message(message&& other)
+        : msg(other.msg) {
+            other.msg = nullptr;
+        }
+        
+        ~message() throw() {
+            if(msg)
+                delete msg;
+        }
+        
+        struct
+        dispatcher {
+            message_base const * msg;
+            
+
+            
+            template <typename LAMBDA>
+            dispatcher
+            handle(LAMBDA handler) const {
+                using message_base_type_for_lambda = typename ::callable::callable_traits<LAMBDA>::template arguments<message_wrapper>;
+                auto msg_proper = dynamic_cast<const message_base_type_for_lambda*>(msg);
+                if(msg_proper) {
+                    ::callable::callable<LAMBDA> c(handler);
+                    c(msg_proper->content);
+                }
+                return dispatcher{msg};
+            }
+        };
+        
+        dispatcher get_dispatcher() const {
+            return dispatcher{msg};
+        }
+    };
+
+}
+
+#endif /* defined(__FunctionalTemplates__message__) */
