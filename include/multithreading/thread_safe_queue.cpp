ad7743a3921049f896193cb02827becb0ae794ea
Julian Becker <becker.julian@gmail.com>
2014-12-10 21:35:12 +0100

    added thread_safe_queue

---
+++ include/multithreading/thread_safe_queue.cpp
@@ -0,0 +1,9 @@
+//
+//  thread_safe_queue.cpp
+//  FunctionalTemplates
+//
+//  Created by Julian Becker on 10.12.14.
+//  Copyright (c) 2014 Julian Becker. All rights reserved.
+//
+
+#include "thread_safe_queue.h"
---
+++ include/multithreading/thread_safe_queue.h
@@ -0,0 +1,44 @@
+//
+//  thread_safe_queue.h
+//  FunctionalTemplates
+//
+//  Created by Julian Becker on 10.12.14.
+//  Copyright (c) 2014 Julian Becker. All rights reserved.
+//
+
+#ifndef __FunctionalTemplates__thread_safe_queue__
+#define __FunctionalTemplates__thread_safe_queue__
+
+#include <queue>
+#include <multithreading/semaphore.h>
+
+namespace multithreading {
+
+    template <typename message> class
+    thread_safe_queue {
+        private: std::queue<message>
+        queue;
+        
+        private: semaphore
+        new_msg_notification;
+        
+        public: void
+        push(message&& msg) {
+            queue.push(std::move(msg));
+            new_msg_notification.post();
+        }
+        
+        public: const message
+        wait_and_pop() {
+            new_msg_notification.wait();
+            auto msg(std::move(queue.front()));
+            queue.pop();
+            return msg;
+        }
+        
+        public: semaphore::interrupt&
+        get_interrupt() const { return new_msg_notification.get_interrupt(); }
+    };
+}
+
+#endif /* defined(__FunctionalTemplates__thread_safe_queue__) */
