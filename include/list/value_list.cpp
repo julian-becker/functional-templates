9eb983cdd5fcfbca0f0bbdf0c863681f569a72f8
Julian Becker <becker.julian@gmail.com>
2014-12-10 21:26:44 +0100

    added value_list header and an int_list to be used in callable::callable template

---
+++ include/list/value_list.cpp
@@ -0,0 +1,9 @@
+//
+//  value_list.cpp
+//  FunctionalTemplates
+//
+//  Created by Julian Becker on 10.12.14.
+//  Copyright (c) 2014 Julian Becker. All rights reserved.
+//
+
+#include "value_list.h"
---
+++ include/list/value_list.h
@@ -0,0 +1,36 @@
+//
+//  value_list.h
+//  FunctionalTemplates
+//
+//  Created by Julian Becker on 10.12.14.
+//  Copyright (c) 2014 Julian Becker. All rights reserved.
+//
+
+#ifndef __FunctionalTemplates__value_list__
+#define __FunctionalTemplates__value_list__
+
+#include <result_of.h>
+
+namespace
+list {
+    
+    template<int...VALUES> struct
+    int_list {
+    };
+
+    template<int N2, int...S> struct
+    make_int_range_list : make_int_range_list<0, N2-1, N2-1, S...> {
+    };
+    
+    template<int N, int...S> struct
+    make_int_range_list<N,N,S...> {
+        using
+        result = int_list<S...>;
+    };
+    
+    template <int N> using
+    int_range_up_to = result_of<make_int_range_list<N>>;
+    
+}
+
+#endif /* defined(__FunctionalTemplates__value_list__) */
