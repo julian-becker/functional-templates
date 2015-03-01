c4cdf9305c9d58906f92a58e00e203c05380d2fb
Julian Becker <becker.julian@gmail.com>
2015-03-01 17:42:53 +0100

    Added some documentation to the meta-programming class "stack"

--- include/words/stack.h
+++ include/words/stack.h
@@ -15,6 +15,7 @@
 namespace
 words {
 
+    //! @cond Doxygen_Suppress
     namespace
     __dtl {
         template <typename STACK, typename...NEW_ELMS> struct
@@ -30,25 +31,39 @@ words {
             using result = typename push_impl<STACK_T<NEW_HEAD,ELMS...>,NEW_ELMS...>::result;
         };
     }
+    //! @endcond Doxygen_Suppress
     
-    template <typename ...ELMS > struct
+    
+    /// @brief: A template metaprogramming stack
+    ///         Do not provide the template arguments directly, instead use
+    ///            stack<>::push< T1, T2, T3>
+    ///         which will first push T1, then T2 and finally T3 onto the stack.
+    template <typename ...> struct
     stack;
     
+    /// @brief: The specialization of the empty stack
     template <> struct
     stack<> {
+        /// @brief: push any number of items onto the stack.
+        ///         The items NEW_ELMS will be pushed in the order listed
         public: template <typename...NEW_ELMS> using
         push = result_of<__dtl::push_impl<stack<>, NEW_ELMS...>>;
     };
     
+    /// @brief: Specialization for the non-empty stack that supports 'pop' and 'top'.
     template <typename HEAD, typename ...ELMS > struct
     stack<HEAD,ELMS...> {
 
+        /// @brief: push any number of items onto the stack.
+        ///         The items NEW_ELMS will be pushed in the order listed
         public: template <typename...NEW_ELMS> using
         push = result_of<__dtl::push_impl<stack<HEAD,ELMS...>, NEW_ELMS...>>;
         
+        /// @brief: Get the top element of the stack.
         public: using
         top = HEAD;
         
+        /// @brief: Drops the top element of the stack.
         public: using
         pop = stack<ELMS...>;
     };
